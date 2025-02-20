e224daf976086a4059e063d46c5488b92a607a42
mjkang4416 <mjkang4416@naver.com>
2025-02-21 03:07:11 +0900

    [chore] docker

--- src/main/java/com/guesthouse/assistpeople/config/SecurityConfig.java
+++ src/main/java/com/guesthouse/assistpeople/config/SecurityConfig.java
@@ -31,8 +31,8 @@ public class SecurityConfig {
 
 
         private static final String[] AUTH_WHITELIST = {
-                "/api/v1/member/**", "/api-docs", "/swagger-ui-custom.html",
-                "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html", "/api/v1/auth/**",
+                         "/api/v1/member/**", "/api-docs",
+                "/v3/api-docs/**", "/api-docs/**", "/api/v1/auth/**",
                 "user/signup", "user/login","/api/swagger-ui/**"
         }; //더 열어둘 엔드포인트 여기에 추가
 
